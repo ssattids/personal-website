bcaea67fe96cbfa19a00a9b55c9377cea3957725
Salar Satti <salarsatti@Salars-MacBook-Pro.local>
2020-04-05 20:31:06 -0400

    Create homepage with pictures and layout

--- personal_website/frontend/personal-website-react/src/App.js
+++ personal_website/frontend/personal-website-react/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {Fragement} from 'react';
 import ReactDOM from 'react-dom';
-
+import NavBar from './components/NavBar';
+import Helmet from 'react-helmet';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import HomePage from './views/HomePage';
 
 const App = () => {
+
+  const theme = React.useMemo(() =>
+      createMuiTheme({
+        palette: {
+          type: 'dark',
+        },
+      }),
+  );
+
   return(
-    <h1>Hello World</h1>
+    <div>
+      <Helmet>
+        <style>{'body { background-color: #000000; }'}</style>
+      </Helmet>
+
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <HomePage />
+      </ThemeProvider>
+    </div>
   )
 }
 
---
+++ personal_website/frontend/personal-website-react/src/views/HomePage/HomePage.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const backgroundImage =
+  'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA23647_hires.jpg';
+
+const profileImage = 
+  "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/57204003_10218197652830778_2031492275145539584_n.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=bQN6WrkwJZgAX_RM_17&_nc_ht=scontent-iad3-1.xx&oh=8d8a593b8da30b4a2651eec1d574dcb4&oe=5EAE3885"
+
+const styles = (theme) => ({
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center',
+  },
+  button: {
+    minWidth: 200,
+  },
+  h2: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(10),
+    },
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(10),
+    },
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+  large: {
+    width: theme.spacing(30),
+    height: theme.spacing(30),
+  },
+});
+
+function ProductHero(props) {
+  const { classes } = props;
+
+  return (
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* Increase the network loading priority of the background image. */}
+      <Avatar alt="Remy Sharp" src={profileImage} className={classes.large} />
+      <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
+      <Typography color="inherit" align="center" variant="h2" marked="center" className={classes.h2}>
+        Salar Satti
+      </Typography>
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        Data Scientist
+      </Typography>
+      <Typography variant="body2" color="inherit" className={classes.more}>
+        Trying to solve the world's problems using data
+      </Typography>
+    </ProductHeroLayout>
+  );
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductHero);
